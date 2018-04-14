375efdfc7e55f7104dae3d76028a866cc9e21ebb
Matt Scheetz <mfscheetz@gmail.com>
2018-04-13 20:00:04 -0400

    Pairs enum and trading pairs object

---
+++ CryptoPortfolio.Business.Contracts/CryptoBits/Pairs.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CryptoPortfolio.Business.Contracts.CryptoBits
+{
+    public enum Pairs
+    {
+        BTC,
+        ETH,
+        NEO,
+        BNB,
+        KCS,
+        USD,
+        USDT,
+    }
+}
---
+++ CryptoPortfolio.Business.Contracts/CryptoBits/TradingPair.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CryptoPortfolio.Business.Contracts.CryptoBits
+{
+    public class TradingPair
+    {
+        public string symbol { get; set; }
+        public string pair { get; set; }
+    }
+}
